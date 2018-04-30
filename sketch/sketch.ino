ad1e6c8aa8bda74bf6cd8e96e182efab31c5fe24
Shane Claussen <claussen@us.ibm.com>
2018-04-30 12:14:13 -0700

    Integrated DHT with HC-SR04

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,16 +1,29 @@
 #include "DHT.h"
 #include "NewPing.h"
 
+// DHT Sensor Pins
+// VCC    5V
+// Data   7
+// GND    GND
+
+// HC-SR04 Ultrasonic Sensor Pins
+// VCC    5V
+// TRIG   11
+// ECHO   13
+// GND    GND
+
 // Constants
+#define DHT_PIN 7
+#define DHT_TYPE DHT22  // aka AM2302
+
 #define ULTRASONIC_SENSOR_TRIGGER_PIN 13
 #define ULTRASONIC_SENSOR_ECHO_PIN 11
-#define DHT_TYPE DHT22
-#define DHT_PIN 7
-#define MAXIMUM_DISTANCE = 400;
+#define MAXIMUM_DISTANCE 400
 
 // Variables
 float humidity;
-float temperature;
+float temperatureCelcius;
+float temperatureFahrenheit;
 float duration;
 float distance;
 float soundMetersPerSecond;
@@ -18,12 +31,10 @@ float soundCentimetersPerMillisecond;
 int iterations = 5;
 
 NewPing sonar(ULTRASONIC_SENSOR_TRIGGER_PIN, ULTRASONIC_SENSOR_ECHO_PIN, MAXIMUM_DISTANCE);
-DHT dht(DHT_PIN, DHTTYPE);
+DHT dht(DHT_PIN, DHT_TYPE);
 
 void setup() {
   Serial.begin(9600);
-  /* pinMode(ultrasonicSensorTriggerPin, OUTPUT); */
-  /* pinMode(ultrasonicSensorEchoPin, INPUT); */
   dht.begin();
 }
 
@@ -31,33 +42,45 @@ void loop() {
   // Delay to enable the DHT-22 to stabalize
   delay(2000);
 
-  // Read the humidity and temperature
+  // Read the humidity and temperature (in C & F)
   humidity = dht.readHumidity();
-  temperature = dht.readTemperature();
+  temperatureCelcius = dht.readTemperature();
+  temperatureFahrenheit = (temperatureCelcius * 1.8) + 32;
 
   // Calculate the speed of sound in meters per second
   // and then convert to centimeters per millisecond
-  soundMetersPerSecond = 331.4 + (0.606 * temperature) + (0.0124 * humidity);
-  soundCentimetersPerMillisecond = soundMetersPerSecond / 10000;
+  soundMetersPerSecond = 331.4 + (0.606 * temperatureCelcius) + (0.0124 * humidity);
 
+  // Perform the ping
   duration = sonar.ping_median(iterations);
 
-  // 0.0343 meters per second is the speed of sound
-  distance = (duration / 2) * .0343;
+  // Print out the results of the sensors
 
-  // Print out the results of the sensor
-  Serial.print("Sound: ");
-  Serial.print(soundMetersPerSecond);
-  Serial.print(" m/s, ");
-  Serial.print("Humidity: ");
-  Serial.print(humidity);
-  Serial.print(" C, ");
-  Serial.print("Distance: ");
+  // Temperature
+  Serial.print("  Celcius: ");
+  Serial.print((int) temperatureCelcius);
+  Serial.print("C");
+  Serial.print("  Fahrenheit: ");
+  Serial.print((int) temperatureFahrenheit);
+  Serial.print("F");
 
+  // Humidity
+  Serial.print("  Humidity: ");
+  Serial.print((int) humidity);
+  Serial.print("%");
+
+  // Speed of Sound
+  Serial.print("  Speed of sound: ");
+  Serial.print((int) soundMetersPerSecond);
+  Serial.print(" m/s");
+
+  // Distance
+  distance = (duration / 2) * (soundMetersPerSecond / 10000);
+  Serial.print("  Distance: ");
   if (distance >= 400 || distance <= 2) {
-    Serial.println("Out of range");
+    Serial.print("OOR");
   } else {
-    Serial.print(distance);
+    Serial.print((int) distance);
     Serial.println(" cm");
   }
 }
